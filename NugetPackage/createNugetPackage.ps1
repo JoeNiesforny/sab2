da9fd0d2cc8b6858ed765779921477b1dc9ed965
Joe <jozwludz@gmail.com>
2016-06-07 22:10:12 +0200

    added nuget package creator by powershell

---
+++ NugetPackage/createNugetPackage.ps1
@@ -0,0 +1,15 @@
+﻿rmdir -Force -Recurse workspace
+
+mkdir workspace\build
+mkdir workspace\content
+mkdir workspace\lib\net45
+mkdir workspace\tools
+
+copy .\Package.nuspec .\workspace\.
+
+copy ..\output\ClusterManager.dll .\workspace\lib\net45\
+copy ..\output\ExtensionForVS.vsix .\workspace\tools\
+copy ..\output\GuntherTesterRunner.exe .\workspace\tools\
+copy -Recurse ..\ExtensionForVS\ .\workspace\build\
+
+.\nuget.exe pack .\workspace\Package.nuspec -Prop Configuration=Release
